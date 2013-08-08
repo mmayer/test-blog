f6307035e35431659934c7434d0b7a4aa0b9a5b2
Markus Mayer <mmayer@mmayer.net>
2013-08-07 21:21:24 -0700

    Fixed site configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,20 +3,20 @@
 
 AUTHOR = 'Markus Mayer'
 SITENAME = 'Pelican Test'
-SITEURL = ''
+SITEURL = 'http://pelican.mmayer.net/'
 
 TIMEZONE = 'America/Vancouver'
 
 DEFAULT_LANG = 'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+#LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#          ('Python.org', 'http://python.org'),
+#          ('Jinja2', 'http://jinja.pocoo.org'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
